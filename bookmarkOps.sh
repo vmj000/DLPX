bc5e98f16d5eaf4ecd93474b86d8fdad182667be
vmj000 <jinuvm@gmail.com>
2018-08-06 00:27:31 -0400

    added branch operations and update bookmarkl ops to show container info

--- bookmarkOps.sh
+++ bookmarkOps.sh
@@ -5,14 +5,9 @@
 # set this to the FQDN or IP address of the Delphix Engine
 DE="172.16.31.131"
 # set this to the Delphix admin user name
-DELPHIX_ADMIN="dev"
+DELPHIX_ADMIN="delphix_admin"
 # set this to the password for the Delphix admin user
-DELPHIX_PASS="delphix"
-# set this to the object reference for the VDB
-VDB="ORACLE_VIRTUAL_SOURCE-6"
-
-
-
+DELPHIX_PASS="landshark"
 
 rm cookies.txt
 
@@ -53,46 +48,79 @@ echo "\n"
 
 case $choice  in
 create)
-  curl -X GET -k http://${DE}/resources/json/delphix/jetstream/branch  -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}'  | grep -E 'reference|name'
 
- echo "Type  only the number from your branch metadata name from above:"
- echo "(Example: If JS_BRANCH-12 then type 12)"
- read brname
-  echo "New Bookmark Name:"
+    curl -X GET -k http://${DE}/resources/json/delphix/jetstream/container -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}'  | grep -E 'reference|name'
+
+ echo "Type  only the number from your container  metadata name from above:"
+ echo "(Example: If JS_DATA_CONTAINER-12 then type 12)"
+ read cntnr
+
+  curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/branch?dataLayout=JS_DATA_CONTAINER-${cntnr}  -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name|dataLayout'
+
+  echo "Below is the list of branches available in your selected container:"
+  echo "You need to select the branch on which you want to create the bookmark:"
+  echo "Type only the number from your branch  referenca name  from top :"
+  echo "(Example: If JS_BRANCH-12 then type 12)"
+  read brname
+  echo " Provide New Bookmark Name:"
   read bkmk
   sh createBookmark.sh ${bkmk}  JS_BRANCH-${brname}
 ;;
 share)
-  curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/bookmark/ \
-    -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name'
+   curl -X GET -k http://${DE}/resources/json/delphix/jetstream/container -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}'  | grep -E 'reference|name'
+
+ echo "Type  only the number from your container  metadata name from above:"
+ echo "(Example: If JS_DATA_CONTAINER-12 then type 12)"
+ read cntnr
+
+  curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/bookmark?dataLayout=JS_DATA_CONTAINER-${cntnr}  -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name|dataLayout'
+
+  echo "Below is the list of bookmarks available in your selected container:"
 
   echo "Type only the number from your bookmark referenca name  from top :"
   echo "(Example: If JS_BOOKMARK-12 then type 12)"
   read BOOKMARK_REF
+
 
 # share  the bookmark
   curl -s -X POST  -k http://${DE}/resources/json/delphix/jetstream/bookmark/JS_BOOKMARK-${BOOKMARK_REF}/share \
     -b ~/cookies.txt -H "Content-Type: application/json" 
 ;;
 unshare)
-    curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/bookmark/ \
-    -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name'
+    curl -X GET -k http://${DE}/resources/json/delphix/jetstream/container -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}'  | grep -E 'reference|name'
+
+ echo "Type  only the number from your container  metadata name from above:"
+ echo "(Example: If JS_DATA_CONTAINER-12 then type 12)"
+ read cntnr
+
+  curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/bookmark?dataLayout=JS_DATA_CONTAINER-${cntnr}  -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name|dataLayout'
+
+  echo "Below is the list of bookmarks available in your selected container:"
 
   echo "Type only the number from your bookmark referenca name  from top :"
   echo "(Example: If JS_BOOKMARK-12 then type 12)"
   read BOOKMARK_REF
+
 
 # unshare  the bookmark
    curl -s -X POST  -k http://${DE}/resources/json/delphix/jetstream/bookmark/JS_BOOKMARK-${BOOKMARK_REF}/unshare \
     -b ~/cookies.txt -H "Content-Type: application/json"
 ;;
 delete)
-    curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/bookmark/ \
-    -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name'
+    curl -X GET -k http://${DE}/resources/json/delphix/jetstream/container -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}'  | grep -E 'reference|name'
+
+ echo "Type  only the number from your container  metadata name from above:"
+ echo "(Example: If JS_DATA_CONTAINER-12 then type 12)"
+ read cntnr
+
+  curl -s -X GET -k http://${DE}/resources/json/delphix/jetstream/bookmark?dataLayout=JS_DATA_CONTAINER-${cntnr}  -b ~/cookies.txt -H "Content-Type: application/json"  | sed -e 's/[{}]/''/g' | awk -v RS=',' -F: '{print $1 $2}' | grep -wE 'reference|name|dataLayout'
+
+  echo "Below is the list of bookmarks available in your selected container:"
 
   echo "Type only the number from your bookmark referenca name  from top :"
   echo "(Example: If JS_BOOKMARK-12 then type 12)"
   read BOOKMARK_REF
+
 
 #delete the bookmark
    curl -s -X DELETE -k http://${DE}/resources/json/delphix/jetstream/bookmark/JS_BOOKMARK-${BOOKMARK_REF} \
